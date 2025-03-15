5954016270e6e0882a8d4c86edee6368320a88dd
Gabriand <gabrielrivera11a@gmail.com>
2025-03-15 17:06:40 -0500

    Nuevo cambio

--- probando/Program.cs
+++ probando/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-string stripe_api_key="sk_test_51J3";
+string stripe_api_key = "sk_test_51J3K8hQ2W7L9mN0P1Q2R3S4";
